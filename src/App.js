0eeb5868f6e7404aa677380cd10ff2710ba51f9b
Fred Jiang <jiangf@shinetechsoftware.com>
2023-05-15 16:05:05 +0800

    change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>AWS Deploy Fred.</p>
+        <p>Hi Fred.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
